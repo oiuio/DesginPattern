3b2e131df844ef737cbc1e8339d2d7fbd54ac92d
oiuio <dxy19900214>
2018-02-23 17:53:42 +0800

    add static factory

--- src/main/java/staticfactory/Creator.java
+++ src/main/java/staticfactory/Creator.java
@@ -5,13 +5,14 @@ public class Creator {
 	private static final String PRODUCT_A = "PRODUCTA";
 	private static final String PRODUCT_B = "PRODUCTB";
 
-	public static IProduct getInstance(String productName){
-		switch (productName.toUpperCase()){
+	public static IProduct getInstance(String productName) {
+		switch (productName.toUpperCase()) {
 			case PRODUCT_A:
 				return new ProductA();
 			case PRODUCT_B:
 				return new ProductB();
-				default: throw new RuntimeException("not found");
+			default:
+				throw new RuntimeException("not found");
 		}
 	}
 
